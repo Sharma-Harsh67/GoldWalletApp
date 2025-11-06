e00c589c1fea539869820f9be39571fd3465d592
Kiranmayee Dhanankula <dhanankula.kiran-mayee@capgemini.com>
2025-11-06 13:03:30 +0530

    Added Dto

---
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/dto/TransactionQuantityDTOKiranmayee.java
@@ -0,0 +1,27 @@
+package com.digitalgoldwallet.dto;
+
+public class TransactionQuantityDTOKiranmayee {
+    private String userName;
+    private Double quantity;
+
+    public TransactionQuantityDTOKiranmayee(String userName, Double quantity) {
+        this.userName = userName;
+        this.quantity = quantity;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/dto/UsersDTOKiranmayee.java
@@ -0,0 +1,29 @@
+package com.digitalgoldwallet.dto;
+
+public class UsersDTOKiranmayee {
+    private String name;
+    private String email;
+
+    
+    public UsersDTOKiranmayee(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
