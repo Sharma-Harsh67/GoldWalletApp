d39bcda2bd52e4ed7c6013abab8dee154644f7ae
Joshika-Kuna <KUNA.JOSHIKA-NAGA-GOWRI@CAPGEMINI.COM>
2025-11-06 13:15:02 +0530

    Add files via upload

---
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/dto/VirtualGoldHoldingControllerJoshika.java
@@ -0,0 +1,39 @@
+package com.wallet.controller;
+
+
+import java.util.List;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.wallet.dto.VirtualGoldHoldingDTOJoshika;
+
+import com.wallet.service.VirtualGoldServiceJoshika;
+
+
+
+@RestController
+@RequestMapping("/api/virtual-gold")
+public class VirtualGoldHoldingControllerJoshika {
+
+
+public VirtualGoldHoldingControllerJoshika(VirtualGoldServiceJoshika goldService) {
+		super();
+		this.goldService = goldService;
+	}
+
+
+
+private VirtualGoldServiceJoshika goldService;
+
+@GetMapping("/holdings-by-name")
+public List<VirtualGoldHoldingDTOJoshika> getHoldingsByName(@RequestParam String name) {
+return goldService.getHoldingsByName(name);
+}
+}
