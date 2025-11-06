84b763f640ea0813f3a2e4c408bb3117e4285fda
Pillala Ganesh <pillala.ganesh@capgemini.com>
2025-11-06 12:32:50 +0530

    Add files via upload

--- Digital-Wallet-Project/src/main/java/com/wallet/app/service/VendorService.java
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/service/VendorService.java
@@ -1,16 +1,20 @@
 package com.wallet.app.service;
 
+import com.wallet.app.dto.BranchSummaryDTO;
 import com.wallet.app.model.Vendor;
+import com.wallet.app.repository.BranchRepository;
 import com.wallet.app.repository.VendorRepository;
-
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class VendorService {
+    private final BranchRepository branchRepository;
     private final VendorRepository vendorRepository;
 
-    public VendorService(VendorRepository vendorRepository) {
+    public VendorService(BranchRepository branchRepository, VendorRepository vendorRepository) {
+        this.branchRepository = branchRepository;
         this.vendorRepository = vendorRepository;
     }
 
@@ -20,5 +24,9 @@ public class VendorService {
 
     public Vendor getVendorById(Long id) {
         return vendorRepository.findById(id).orElse(null);
+    }
+
+    public List<BranchSummaryDTO> getBranchSummary() {
+        return branchRepository.getBranchSummary();
     }
 }
