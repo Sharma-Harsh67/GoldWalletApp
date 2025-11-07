c632fea889ba6f1bf1b3fc16b30605e4eeb01a10
Jagathesswaran-vc18 <jagathesswaran.v@capgemini.com>
2025-11-07 10:21:48 +0530

    Update UserRepositoryJagat

--- Digital-Wallet-Project/src/main/java/com/wallet/app/repository/UserRepositoryJagat.java
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/repository/UserRepositoryJagat.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepositoryJagat extends JpaRepository<UserJagat, Integer> {
-    // additional queries if needed
-
-
-}
+public interface UserRepositoryJagat extends JpaRepository<UserJagat, Integer> {}
