da1552333a0505db07bc3ad38aa000d2f351a1cc
Pillala Ganesh <pillala.ganesh@capgemini.com>
2025-11-07 15:15:10 +0530

    Delete Digital-Wallet-Project/src/main/java/com/wallet/app/service/VirtualGoldServiceJoshika.java

--- Digital-Wallet-Project/src/main/java/com/wallet/app/service/VirtualGoldServiceJoshika.java
+++
@@ -1,9 +0,0 @@
-package com.wallet.app.service;
-
-import java.util.List;
-
-import com.wallet.app.dto.VirtualGoldHoldingDTOJoshika;
-
-public interface VirtualGoldServiceJoshika {
-	List<VirtualGoldHoldingDTOJoshika> getHoldingsByName(String name);
-}
