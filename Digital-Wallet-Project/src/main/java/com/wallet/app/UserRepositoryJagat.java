b1c6119d599b223ab25b30d54ad70fd085ebb75c
Abdullah12871 <abdullah.abdullah@capgemini.com>
2025-11-07 10:02:42 +0530

    updating jagat repo

---
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/UserRepositoryJagat.java
@@ -0,0 +1,12 @@
+package com.wallet.app.repository;
+
+import com.wallet.app.model.UserJagat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepositoryJagat extends JpaRepository<UserJagat, Integer> {
+    // additional queries if needed
+
+
+}
