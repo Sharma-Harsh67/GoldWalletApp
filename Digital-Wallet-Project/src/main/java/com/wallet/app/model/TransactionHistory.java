cf87d9a55faaec353d403d8081614069626d6d06
Pillala Ganesh <pillala.ganesh@capgemini.com>
2025-11-07 15:56:02 +0530

    semifinal-model

--- Digital-Wallet-Project/src/main/java/com/wallet/app/model/TransactionHistory.java
+++ Digital-Wallet-Project/src/main/java/com/wallet/app/model/TransactionHistory.java
@@ -2,6 +2,9 @@ package com.wallet.app.model;
 
 import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.wallet.app.enums.TransactionStatus;
+import com.wallet.app.enums.TransactionType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,8 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
- 
- 
+
 @Entity
 @Table(name = "transaction_history")
 public class TransactionHistory {
@@ -28,47 +30,83 @@ public class TransactionHistory {
 	private Double quantity;
 	@Column(name = "branch_id")
 	private Integer branchId;
+	@Column(name = "amount")
+	private double amount;
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
- 
+
+
+@Column(name = "transaction_type")
+private TransactionType transactionType;
+
+@Column(name = "transaction_status")
+private TransactionStatus transactionStatus;
+
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
 	public Integer getTransactionId() {
 		return transactionId;
 	}
- 
+
 	public void setTransactionId(Integer transactionId) {
 		this.transactionId = transactionId;
 	}
- 
+
 	public Users getUser() {
 		return user;
 	}
- 
+
 	public void setUser(Users user) {
 		this.user = user;
 	}
- 
+
 	public Double getQuantity() {
 		return quantity;
 	}
- 
+
+	
+
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(TransactionType transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public TransactionStatus getTransactionStatus() {
+		return transactionStatus;
+	}
+
+	public void setTransactionStatus(TransactionStatus transactionStatus) {
+		this.transactionStatus = transactionStatus;
+	}
+
 	public void setQuantity(Double quantity) {
 		this.quantity = quantity;
 	}
- 
+
 	public Integer getBranchId() {
 		return branchId;
 	}
- 
+
 	public void setBranchId(Integer branchId) {
 		this.branchId = branchId;
 	}
- 
+
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
- 
+
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
- 
+
 }
